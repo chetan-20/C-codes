b1dbd6ef7bf60935caa55239a308cc96ddecd4df
chetan-20 <36814738+chetan-20@users.noreply.github.com>
2018-03-06 21:17:08 +0530

    Add files via upload

---
+++ To check whether a variable.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+
+{
+char ch;
+printf("Enter the character");
+scanf("%c",&ch);
+if((ch>='a'&&ch<='z')||(ch>='A'&&ch<='Z'))
+  {  printf("Enter variable is a variable");
+}
+else
+{ printf("Enter variable is not a varaible\n");
+}
+
+
+}
+
+
+
+/*
+output:
+
+Enter the charactera
+Enter variable is a variabledbit@elab2-16:~/Desktop/chetan$
+
+
+
+*/
+
