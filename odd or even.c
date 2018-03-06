bed3ad1f54f528781b620e1a598e3484449f3459
chetan-20 <36814738+chetan-20@users.noreply.github.com>
2018-03-06 20:42:22 +0530

    Add files via upload

---
+++ odd or even.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int main()
+
+{
+
+
+int number;
+
+
+printf("input integer:\n");
+
+scanf("%d",&number);
+
+
+if (number %2==0 )
+    
+printf("%d is an even number",number);
+
+else
+   
+printf("%d is an odd number",number);
+
+
+}
+
+
