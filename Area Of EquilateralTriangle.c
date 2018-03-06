dd4bb9a7f2b33131d8d78f8208d3b55585f85d1c
chetan-20 <36814738+chetan-20@users.noreply.github.com>
2018-03-06 20:59:59 +0530

    Add files via upload

---
+++ Area Of EquilateralTriangle.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+
+{
+float a;
+float area;
+ printf("enter the value of side:\n");
+scanf("%f",&a);
+
+area =((sqrt(3))/4)*(a*a);
+
+printf("the area of the equilateral triangle is:%f\n",area);
+
+}
+
